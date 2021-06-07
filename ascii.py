951a2d1b0818eeaa70a94bad4645ee728dc3bcc2
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-06-07 22:33:25 +0800

    Project done

---
+++ ascii.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+chars = " .'`\",:;<>~?!@#$%^&*()_-+=*-/+"
+
+char_width = 18
+char_height = 18
+
+img = Image.open('img.jpg')
+
+WIDTH, HEIGHT = img.size
+img = img.resize(
+    (int(WIDTH / char_width), int(HEIGHT / char_height)), Image.NEAREST)
+width, height = img.size
+img = img.load()
+
+ascii_img = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
+
+fnt = ImageFont.truetype('C:/Windows/Fonts/Consola.ttf', 20)
+
+d = ImageDraw.Draw(ascii_img)
+
+
+def getChar(value):
+    return chars[int(value * (len(chars) / 256))]
+
+
+for i in range(height):
+    for j in range(width):
+        r, g, b = img[j, i]
+        k = int((r + g + b) / 3)
+        d.text((j * char_width, i * char_height),
+               getChar(k), font=fnt, fill=(r, g, b))
+        print(d.text)
+ascii_img.save('output.png')
